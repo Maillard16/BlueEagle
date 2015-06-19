13be3d1e55cc9a9d14adabd20df58bc9da1ad75f
Auriana <auriana.hug@heig-vd.ch>
2015-06-19 10:21:34 +0200

    cleaned ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,21 +6,18 @@ class Ability
    user ||= User.new # guest user (not logged in)
    if user.has_role? :admin 
 		  can :manage, :all
-		  can :menu_admin, @user
 		  can :manage, User do |un_user|
             !un_user.has_role?(:admin) ||         
             un_user.id==user.id
          end
    end
 	 if user.has_role? :blueEagle     
-		  can :menu_blueEagle, @ordinateurs
 		  can [:read, :create, :update], Ordinateur, RendezVou 
 	 end
 	 if user.has_role? :entreprise   
 		 can :read, Ordinateur    
 	 end
 	 if user.has_role? :organisme
-		 can :menu_organisme, @rendez-vous
 		 can [:read, :create, :update], [RendezVou, Beneficiaire]           
 	 end
  end
